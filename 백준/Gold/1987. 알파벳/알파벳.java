ae889a2e491d96cbc5c8d1c0a606d01994228f70
Hyunjoo-J <77290958+Hyunjoo-J@users.noreply.github.com>
2023-08-20 19:22:28 +0900

    [Gold IV] Title: 알파벳, Time: 908 ms, Memory: 12688 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -4,7 +4,7 @@ public class Main {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
     static int r, c, ans;
-    static int[][] board;
+    static char[][] board;
     static boolean[] alpha = new boolean[26];
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,33 +12,31 @@ public class Main {
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
-        board = new int[r][c];
+        board = new char[r][c];
         alpha = new boolean[26];
         for(int i=0; i<r; i++) {
             String[] line = br.readLine().split("");
             for(int j=0; j<line.length; j++) {
                 String alpha = line[j];
-                board[i][j] = alpha.charAt(0)-'A';
+                board[i][j] = alpha.charAt(0);
             }
         }
-        dfs(0,0,0);
+        alpha[board[0][0] - 'A'] = true;
+        dfs(0,0,1);
         System.out.println(ans);
     }
     private static void dfs(int x, int y, int sum){
-        if(alpha[board[x][y]]) {
-            ans = Math.max(ans, sum);
-            return;
-        }
-        else {
-            alpha[board[x][y]] = true;
-            for(int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+        ans = Math.max(ans, sum);
+        int nx, ny;
+        for(int i = 0; i < 4; ++i){
+            nx = x + dx[i];
+            ny = y + dy[i];
 
-                if(nx<0 || ny <0 || nx >r-1 || ny > c-1) continue;
-                dfs(nx, ny, sum+1);
-            }
-            alpha[board[x][y]] = false;
+            if(nx < 0 || nx >= r || ny < 0 || ny >= c || alpha[board[nx][ny] - 'A'])
+                continue;
+            alpha[board[nx][ny] - 'A'] = true;
+            dfs(nx, ny, sum + 1);
+            alpha[board[nx][ny] - 'A'] = false;
         }
     }
 }
