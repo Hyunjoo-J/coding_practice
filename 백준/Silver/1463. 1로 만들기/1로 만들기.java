da52120ba87f5b196e4d58b5f5597dc702aa8734
Hyunjoo-J <77290958+Hyunjoo-J@users.noreply.github.com>
2023-08-31 14:41:18 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 15724 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,23 @@
 import java.io.*;
-import java.util.*;
+public class Main {
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[n + 3];
-        dp[2] = 1;
-        dp[3] = 1;
-        for (int i = 4; i <= n; i++){
-            if(i % 6 == 0)
-                dp[i] = Math.min(dp[i/2], Math.min(dp[i/3], dp[i/2])) + 1;
-            else if (i % 2 == 0)
-                dp[i] = Math.min(dp[i/2], dp[i - 1]) + 1;
-            else if (i % 3 == 0)
-                dp[i] = Math.min(dp[i/3], dp[i - 1]) + 1;
-            else
-                dp[i] = dp[i - 1] + 1;
-        }
-        System.out.println(dp[n]);
-        
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 3];
+		dp[2] = 1;
+		dp[3] = 1;
+		for(int i = 4; i <= N; ++i) {
+			if(i % 6 == 0)
+				dp[i] = Math.min(dp[i/2], Math.min(dp[i/3], dp[i - 1])) + 1;
+			else if(i % 3 == 0)
+				dp[i] = Math.min(dp[i - 1], dp[i/3]) + 1;
+			else if(i % 2 == 0)
+				dp[i] = Math.min(dp[i - 1], dp[i/2]) + 1;
+			else 
+				dp[i] = dp[i - 1] + 1;
+		}
+		System.out.println(dp[N]);
+	}
+
 }
