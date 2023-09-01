19c2aecd4218b11a99be6b16a98229d0146f9104
Hyunjoo-J <77290958+Hyunjoo-J@users.noreply.github.com>
2023-09-01 23:13:13 +0900

    Update 쿨한 물건 구매.java

--- 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.java
+++ 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.java
@@ -17,18 +17,18 @@ public class Main {
             P = Q;
             Q = tmp;
         }
-        //ax + by = c꼴 생각
+        //ax + by = c꼴 생각 실제는 ax + by >= c를 찾아야 함 a = P, b = Q
         int mP = D / P + 1;
         int ans = P * mP;
         for(int i = mP - 1; i >= 0; --i){
-            int div = (D - (P * i)) / Q;
+            int div = (D - (P * i)) / Q; // c - ax 진행 후 남은 부분에서 Q로 나눠지는 몫을 찾음
             int mod = (D - (P * i)) % Q;
-            if(mod == 0){
+            if(mod == 0){//ax + by = c 성립
                 System.out.println(D);
                 return;
             }
-            int tmp = P * i + Q * (div + 1);
-            if(ans == tmp)
+            int tmp = P * i + Q * (div + 1); //Q * div < D - P * i, 그러나 Q * div >= D - P * i를 찾아야 하기에 div + 1을 곱함
+            if(ans == tmp) //우연히 i, div + 1은 다르지만 계산한 결과가 같은 것이 나올 수 있음
                 break;
             ans = Math.min(ans, tmp);
         }
