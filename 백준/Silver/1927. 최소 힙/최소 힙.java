7f98cd48dd1ac35b8ebd7e648a870512891bd64b
Hyunjoo-J <77290958+Hyunjoo-J@users.noreply.github.com>
2023-09-09 15:51:59 +0900

    [Silver II] Title: 최소 힙, Time: 628 ms, Memory: 24656 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int[] heap = new int[100000];
+    static int size = 0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; ++i){
+            int command = Integer.parseInt(br.readLine());
+            if(command == 0){
+                if(size == 0)
+                    sb.append("0\n");
+                else{
+                    sb.append(findmin()+"\n");
+                }
+            }else{
+                heap[size++] = command;
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+    }
+
+    private static int findmin() {
+        int min = heap[0];
+        int idx = 0;
+        for(int i = 1; i < size; ++i){
+            if(min > heap[i]){
+                min = heap[i];
+                idx = i;
+            }
+        }
+        heap[idx] = heap[--size];
+        return min;
+    }
+}
