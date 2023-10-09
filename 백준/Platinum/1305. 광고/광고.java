77db07b99b210e8b2d6f635b8341026a8667607e
Hyunjoo-J <77290958+Hyunjoo-J@users.noreply.github.com>
2023-10-10 00:43:40 +0900

    [Platinum IV] Title: 광고, Time: 180 ms, Memory: 26252 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        char[] txt = br.readLine().toCharArray();
+        int[] t = new int[L];
+        for(int i = 1, j = 0; i < L; ++i){
+            while(j > 0 && txt[i] != txt[j])
+                j = t[j - 1];
+            if(txt[i] == txt[j])
+                t[i] = ++j;
+        }
+        System.out.println(L - t[L - 1]);
+    }
+}
