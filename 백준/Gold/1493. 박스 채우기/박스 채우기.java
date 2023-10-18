03d471b28aa3433ca01b750074ecdf16e385d860
Hyunjoo-J <77290958+Hyunjoo-J@users.noreply.github.com>
2023-10-19 00:07:35 +0900

    [Gold III] Title: 박스 채우기, Time: 80 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Gold/1493. 박스 채우기/박스 채우기.java
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.java
@@ -1,45 +1,42 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
-
 public class Main {
+    static int length, width, height;
+    static long[] cube;
+    static int[] box;
+    static long ans = 1;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        box = new int[3];
+        for(int i=0; i<3; i++) {
+            box[i] = Integer.parseInt(st.nextToken());
+            ans *= box[i];
+        }
+        int N = Integer.parseInt(br.readLine());
+        cube = new long[N];
+        for(int i = 0; i < N; ++i){
+            st = new StringTokenizer(br.readLine());
+            cube[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+        }
+//        ans = length * width * height;
+        System.out.println(fillBox(0, 0, N - 1));
+    }
 
-	static int[] box, cube;
-	static long fullV;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		box = new int[3];
-		fullV = 1;
-		for(int i=0; i<3; i++) {
-			box[i] = Integer.parseInt(st.nextToken());
-			fullV *= box[i];
-		}
-		int n = Integer.parseInt(br.readLine());
-		
-		cube = new int[n];
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			cube[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
-		}
-		System.out.println(solve(n-1, 0, 0));
-	}
-	
-	static long solve(int blockSize, long fill, long cnt) {
-		long totalV = 1;
-		for(int i=0; i<3; i++) {
-			totalV *= box[i] >> blockSize;
-		}
-		
-		int curCube = cube[blockSize];
-		totalV -= fill;
-		long fillCube = Math.min(curCube, totalV);
-		if(blockSize == 0) {
-			if((fill + fillCube) != fullV) {
-				return -1;
-			}
-			return cnt+fillCube;
-		}else {
-			return solve(blockSize-1, fill+fillCube<<3, cnt+fillCube);	
-		}
-	}
+    private static long fillBox(long cnt, long used, int size) {
+        long num = 1;
+        for(int i = 0; i < 3; ++i)
+            num *= box[i] >> size;
+        num -= used;
+        long possibility =  Math.min(cube[size], num);
+        used += possibility;
+        cnt += possibility;
+        if(size == 0){
+            if(used == ans)
+                return cnt;
+            else
+                return -1;
+        }else
+            return fillBox(cnt, used << 3, size - 1);
+    }
 }
