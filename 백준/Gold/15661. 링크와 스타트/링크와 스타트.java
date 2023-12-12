3de6c99bf0f0b4cba6192b3b3b85b33b09dd5df5
Hyunjoo-J <77290958+Hyunjoo-J@users.noreply.github.com>
2023-12-13 02:49:57 +0900

    [Gold V] Title: 링크와 스타트, Time: 904 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,47 +1,51 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    static int N, ans = Integer.MAX_VALUE;
+    static int N;
+    static int ans = Integer.MAX_VALUE;
     static int[][] player;
-    static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine()); //사람의 수
         player = new int[N][N];
-        visit = new boolean[N];
-        for(int i = 0; i < N; ++i){
+        for (int i = 0; i < N; ++i) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; ++j){
+            for (int j = 0; j < N; ++j) {
                 player[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        dfs(0);
+        dfs(0, 0);
         System.out.println(ans);
     }
 
-    private static void dfs(int dep){
-        if(dep == N){
+    private static void dfs(int dep, int bit) {
+        if ((bit & (-bit)) > 0) {
             int start = 0;
             int link = 0;
-            for(int i = 0; i < N; ++i){
-                for(int j = i + 1; j < N; ++j){
-                    if(visit[i] != visit[j]) continue;
-                    if(visit[i])
-                        start += player[i][j] + player[j][i];
-                    else
-                        link += player[i][j] + player[j][i];
+            for (int i = 0; i < N; ++i) {
+                if ((bit & (1 << i)) > 0) {
+                    for (int j = i + 1; j < N; ++j) {
+                        if ((bit & (1 << j)) > 0) {
+                            start += player[i][j];
+                            start += player[j][i];
+                        }
+                    }
+                } else {
+                    for (int j = i + 1; j < N; ++j) {
+                        if ((bit & (1 << j)) == 0) {
+                            link += player[i][j];
+                            link += player[j][i];
+                        }
+                    }
                 }
             }
-
-            int diff = Math.abs(start - link);
-            ans = Math.min(ans, diff);
-            return;
+            ans = Math.min(ans, Math.abs(start - link));
         }
-        visit[dep] = true;
-        dfs(dep + 1);
-        visit[dep] = false;
-        dfs(dep + 1);
+        for (int i = dep; i < N; ++i) {
+            dfs(i + 1, bit | (1 << i));
+        }
     }
 }
